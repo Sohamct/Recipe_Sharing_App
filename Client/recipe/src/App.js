4f72b79f012d5c4503abb71ea5594cbe0aff2b31
Kevin Thumbar <99701249+kevinthumbar@users.noreply.github.com>
2024-04-02 14:03:46 +0530

    fixed some ui design

--- Client/recipe/src/App.js
+++ Client/recipe/src/App.js
@@ -6,36 +6,36 @@ import { RootLayout } from './components/Recipes/RootLayout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Signup } from './components/Auth/Signup';
 import { Login } from './components/Auth/Login';
-import { RecipeShow} from './components/Recipes/RecipeList/RecipeItem/RecipeShow';
+import { RecipeShow } from './components/Recipes/RecipeList/RecipeItem/RecipeShow';
 import { CreateRecipe } from './components/Recipes/CreateRecipe/CreateRecipe';
 import LogoutInfoPage from './components/Auth/LogoutInfoPage';
 import { Chat } from './components/Chat/Chat';
-import UserProfile from  './components/User/userProfile';
+import UserProfile from './components/User/userProfile';
 import FavoriteRecipesPage from './components/Favorites/FavoriteRecipePage.js';
 import { SearchResults } from './components/Recipes/SearchRecipe/SearchResults.js';
 import { SearchRecipe } from './components/Recipes/SearchRecipe/SearchRecipe.js';
-import {MyRecipe} from './components/MyRecipe/MyRecipe';
- 
+import { MyRecipe } from './components/MyRecipe/MyRecipe';
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Navigate to="/recipe" replace />} />
-      <Route path="/recipe" element={<RootLayout />}/>
+      <Route path="/recipe" element={<RootLayout />} />
       <Route path="newrecipe" element={<CreateRecipe />} />
-      <Route path="/editrecipe/:id" element={<CreateRecipe/>}/>
-      <Route path="/recipe" element={<RootLayout />}/>
+      <Route path="/editrecipe/:id" element={<CreateRecipe />} />
+      <Route path="/recipe" element={<RootLayout />} />
       <Route path="newrecipe" element={<CreateRecipe />} />
-      <Route path="/editrecipe/:id" element={<CreateRecipe/>}/>
+      <Route path="/editrecipe/:id" element={<CreateRecipe />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/logout" element={<LogoutInfoPage/>}/>
-      <Route path="/user-profile/:username" element={<UserProfile/>} />
-      <Route path="viewrecipe/:id" element={<RecipeShow />}/>
-      <Route path="/search-results" element={<SearchResults/>} />
+      <Route path="/logout" element={<LogoutInfoPage />} />
+      <Route path="/user-profile/:username" element={<UserProfile />} />
+      <Route path="viewrecipe/:id" element={<RecipeShow />} />
+      <Route path="/search-results" element={<SearchResults />} />
       <Route path="/favorite" element={<FavoriteRecipesPage />} />
-        <Route path="/" element={<SearchRecipe/>} />
-      <Route path="/chat" element={<Chat/>}></Route>
-      <Route path='myrecipe' element={<MyRecipe/>}> </Route>
+      <Route path="/" element={<SearchRecipe />} />
+      <Route path="/chat" element={<Chat />}></Route>
+      <Route path='myrecipe' element={<MyRecipe />}> </Route>
     </Route>
   )
 );
